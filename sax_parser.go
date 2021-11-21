9cfc294fe17c0ce15b94d20edae4ed6318ae4577
Gergely Nagy <nagygr@gmail.com>
2021-11-21 01:16:26 +0100

    ADD Added parser with test data and test.

--- sax_parser.go
+++ sax_parser.go
@@ -2,6 +2,7 @@ package saxparser
 
 import (
 	"encoding/xml"
+	"errors"
 	"io"
 )
 
@@ -15,4 +16,36 @@ func NewParser(reader io.Reader, handler SaxHandler) *SaxParser {
 		decoder: xml.NewDecoder(reader),
 		handler: handler,
 	}
+}
+
+func (p *SaxParser) parse() error {
+	for token, err := p.decoder.Token(); err != io.EOF; token, err = p.decoder.Token() {
+		if err != nil {
+			return err
+		}
+
+		switch t := token.(type) {
+			case xml.CharData:
+				p.handler.Characters(t)
+			case xml.StartElement:
+				name := t.Name.Local
+				attrs := make(map[string]string, len(t.Attr))
+				for _, v := range t.Attr {
+					attrs[v.Name.Local] = v.Value
+				}
+				p.handler.StartElement(name, attrs)
+			case xml.EndElement:
+				p.handler.EndElement(t.Name.Local)
+			case xml.Comment:
+				p.handler.Comment(t)
+			case xml.ProcInst:
+				p.handler.ProcessingInstruction(t.Target, t.Inst)
+			case xml.Directive:
+				p.handler.Directive(t)
+			default:
+				return errors.New("Unknown XML tag")
+		}
+	}
+
+	return nil
 }
--- sax_parser_test.go
+++ sax_parser_test.go
@@ -1,36 +1,77 @@
 package saxparser
 
 import (
-	"fmt"
 	"testing"
+	"os"
+	"strings"
 )
 
-type SaxHandlerTest struct {}
+type SaxHandlerTest struct {
+	t *testing.T
+	inApple bool
+}
 
 func (h *SaxHandlerTest) Characters(chars []byte) {
-	fmt.Printf("Characters: %s\n", string(chars))
+	text := string(chars)
+
+	if strings.TrimSpace(text) == "" {
+		return
+	}
+
+	expected := "sieversii"
+
+	if text != expected {
+		h.t.Errorf("Characters: the characters (%s) are not equal to what was expected (%s)", text, expected)
+	}
 }
 
 func (h *SaxHandlerTest) Comment(chars []byte) {
-	fmt.Printf("Comment: %s\n", string(chars))
+	text := string(chars)
+	expected := " comment "
+
+	if text != expected {
+		h.t.Errorf("Comment: the characters (%s) are not equal to what was expected (%s)", text, expected)
+	}
 }
 
 func (h *SaxHandlerTest) Directive(chars []byte) {
-	fmt.Printf("Directive: %s\n", string(chars))
+	text := string(chars)
+	expected := "DOCTYPE apple SYSTEM \"example.dtd\""
+
+	if text != expected {
+		h.t.Errorf("Directive: the characters (%s) are not equal to what was expected (%s)", text, expected)
+	}
 }
 
 func (h *SaxHandlerTest) EndElement(name string) {
-	fmt.Printf("End element: %s\n", name)
+	if h.inApple {
+		h.inApple = false
+		if name != "attrs" {
+			h.t.Errorf("End element: name (%s) is not what was expected (attrs)", name)
+		}
+	} else if name != "apple" {
+		h.t.Errorf("End element: name (%s) is not what was expected (apple)", name)
+	}
 }
 
 func (h *SaxHandlerTest) ProcessingInstruction(target string, instruction []byte) {
-	fmt.Printf("Target: %s Instruction: %s\n", target, string(instruction))
+	if !(target == "xml" && string(instruction) == "version='1.0' encoding='UTF-8'") {
+		h.t.Errorf("ProcessingInstruction not what was expected")
+	}
 }
 
 func (h *SaxHandlerTest) StartElement(name string, attributes map[string]string) {
-	fmt.Printf("Start element: %s\n", name)
-	for key, value := range attributes {
-		fmt.Printf("\tAttribute: %s %s", key, value)
+	if name == "apple" {
+		h.inApple = true
+	} else if name == "attrs" {
+		if value, present := attributes["country"]; !present || value != "China" {
+			h.t.Errorf("Missing attribute: country='China'")
+		}
+		if value, present := attributes["region"]; !present || value != "Tien Shan" {
+			h.t.Errorf("Missing attribute: region='Tien Shan'")
+		}
+	} else {
+		h.t.Errorf("Unexpected element: %s", name)
 	}
 }
 
@@ -41,7 +82,7 @@ func (r* ReaderTest) Read(buf []byte) (n int, err error) {
 }
 
 func TestNewParser(t *testing.T) {
-	parser := NewParser(&ReaderTest{}, &SaxHandlerTest{})
+	parser := NewParser(&ReaderTest{}, &SaxHandlerTest{t:t})
 
 	if parser.decoder == nil {
 		t.Errorf("SaxParser.decoder is nil")
@@ -50,4 +91,15 @@ func TestNewParser(t *testing.T) {
 	if _, ok := parser.handler.(SaxHandler); !ok {
 		t.Errorf("SaxParser.handler is not a SaxParser")
 	}
+}
+
+func TestExampleXml(t *testing.T) {
+	file, err := os.Open("test_data/example.xml")
+
+	if err != nil {
+		t.Errorf("Error opening example.xml: %s", err.Error())
+	}
+
+	parser := NewParser(file, &SaxHandlerTest{t:t})
+	parser.parse()
 }
