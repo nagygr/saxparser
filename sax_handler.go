fc7fe7577e82e69cf81f5db66c31dacc1b7c1706
Gergely Nagy <nagygr@gmail.com>
2021-11-22 00:03:32 +0100

    ADD Adding package documentation.

--- sax_handler.go
+++ sax_handler.go
@@ -1,10 +1,46 @@
 package saxparser
 
+/*
+SaxHandler is an interface for the callback functions for the SAX parser.
+It defines a function for each XML element type. By implementing the
+interface, an XML document can be processed as a series of events. If the
+implementing struct is given state, it can act as a state machine that is
+able determine the current position is a well-formed XML and act accordingly.
+*/
 type SaxHandler interface {
+	/*
+	Characters is a handler function for character data -- i.e. the characters
+	between and opening and a closing tag. Please note, that any whitespace between
+	two tags is considered to be character data so a lot of these callback will be made
+	for formatting spaces. This is relatively easy to filter out though.
+	*/
 	Characters(chars []byte)
+
+	/*
+	Comment is the callback function for comments (the starting and ending symbols are
+	not included).
+	*/
 	Comment(chars []byte)
+
+	/*
+	Directive is the callback function for XML directives: <!directive ...>.
+	*/
 	Directive(chars []byte)
+
+	/*
+	EndElement is the callback function for end elements.
+	*/
 	EndElement(name string)
+
+	/*
+	ProcessingInstruction is the callback function for procInsts, i.e.
+	elements in the form of: <?...>
+	*/
 	ProcessingInstruction(target string, instruction []byte)
+
+	/*
+	StartElement is the callback function for start elements. The name of
+	the element and a map of attributes is given.
+	*/
 	StartElement(name string, attributes map[string]string)
 }
--- sax_parser.go
+++ sax_parser.go
@@ -1,3 +1,7 @@
+/*
+Package saxparser is a very thin wrapper over encoding/xml providing a SAX-like
+API with callback functions corresponding to all the XML element types.
+*/
 package saxparser
 
 import (
@@ -5,11 +9,19 @@ import (
 	"io"
 )
 
+/*
+SaxParser is the struct encompassing the XML parser. It holds a pointer to an
+xml.Decoder and the SaxHandler implementation.
+*/
 type SaxParser struct {
 	decoder *xml.Decoder
 	handler SaxHandler
 }
 
+/*
+NewParser creates a new SaxParser. It expects an io.Reader for the XML document
+and a SaxHandler implementation.
+*/
 func NewParser(reader io.Reader, handler SaxHandler) *SaxParser {
 	return &SaxParser{
 		decoder: xml.NewDecoder(reader),
@@ -17,6 +29,12 @@ func NewParser(reader io.Reader, handler SaxHandler) *SaxParser {
 	}
 }
 
+/*
+Parse performs the actual parsing of the XML document. It processes the XML
+file, calls the callback functions of the provided SaxHandler implementation and
+returns an error if anything goes wrong -- it simply forwards the errors of
+encoding/xml.Decoder.Token().
+*/
 func (p *SaxParser) Parse() error {
 	for token, err := p.decoder.Token(); err != io.EOF; token, err = p.decoder.Token() {
 		if err != nil {
